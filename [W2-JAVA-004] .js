e600e1a62ded5761f5fcb998d91a4c4e673e28ce
LOGXSTAR <reno@logxstar.com>
2018-05-13 21:51:55 +0200

    [W2-JAVA-004]
    Arrays

---
+++ [W2-JAVA-004] .js
@@ -0,0 +1,24 @@
+// Arrays
+
+function string(start, end, step = start < end ? 1 : -1) {
+  let array = [];
+
+  if (step > 0) {
+    for (let i = start; i <= end; i += step) array.push(i);
+  } else {
+    for (let i = start; i >= end; i += step) array.push(i);
+  }
+  return array;
+}
+
+function sum(array) {
+  let total = 0;
+  for (let value of array) {
+    total += value;
+  }
+  return total;
+}
+
+console.log(string(1, 10))
+console.log(string(5, 2, -1));
+console.log(sum(string(1, 10)));
